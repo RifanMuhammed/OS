7e74fc6d69247b351ea945970fe980e4f4b04e44
Joe <127773439+joegeorge022@users.noreply.github.com>
2026-05-24 20:28:31 +0530

    Update 6.SJF.c

--- 6.SJF.c
+++ 6.SJF.c
@@ -2,23 +2,21 @@
 
 int main() {
     int n, i, time = 0, completed = 0;
-    int bt[10], at[10], wt[10], done[10] = {0};
+    int bt[10], at[10], wt[10], tat[10], ct[10], done[10] = {0};
 
     printf("Enter number of processes: ");
     scanf("%d", &n);
 
     for(i = 0; i < n; i++) {
-        printf("P%d Burst Arrival: ", i + 1);
+        printf("P%d Burst Arrival: ", i+1);
         scanf("%d %d", &bt[i], &at[i]);
     }
 //
     while(completed < n) {
-        int min = 9999;
-        int idx = -1;
+        int min = 9999, idx = -1;
 
         for(i = 0; i < n; i++) {
-
-            if(at[i] <= time && done[i] == 0 && bt[i] < min) {
+            if(at[i] <= time && !done[i] && bt[i] < min) {
                 min = bt[i];
                 idx = i;
             }
@@ -26,20 +24,20 @@ int main() {
 
         if(idx != -1) {
             wt[idx] = time - at[idx];
-            time = time + bt[idx];
+            time += bt[idx];
+            ct[idx] = time;
+            tat[idx] = ct[idx] - at[idx];
             done[idx] = 1;
             completed++;
-        }
-        else {
+        } else {
             time++;
         }
     }
 //
     float avg = 0;
-
-    for(i = 0; i < n; i++)
+    for(i = 0; i < n; i++) {
         avg += wt[i];
-
+    }
     avg /= n;
 
     printf("\nAvg Waiting Time = %.2f\n", avg);
