d7002f224ba1cfc79b672495565e7b1f7fc0f3b7
Joe <127773439+joegeorge022@users.noreply.github.com>
2026-05-26 00:49:44 +0530

    Update 10.SSTF.c

--- 10.SSTF.c
+++ 10.SSTF.c
@@ -2,36 +2,33 @@
 #include <stdlib.h>
 
 int main() {
-    int n, i, head, total = 0;
+    int n, head, total = 0;
     int req[20], visited[20] = {0};
 
     printf("Enter number of requests: ");
     scanf("%d", &n);
 
     printf("Enter requests: ");
-    for(i = 0; i < n; i++) scanf("%d", &req[i]);
+    for(int i = 0; i < n; i++){
+        scanf("%d", &req[i]);
+    }
 
     printf("Enter initial head position: ");
     scanf("%d", &head);
 
-    for(i = 0; i < n; i++) {
-        int min = 9999, idx = -1;
+    for(int i = 0; i < n; i++) {
+        int min = 9999,pos = -1;
 
         for(int j = 0; j < n; j++) {
-            if(!visited[j]) {
-                int dist = abs(head - req[j]);
-                if(dist < min) {
-                    min = dist;
-                    idx = j;
-                }
+            int diff = abs(head - req[j]);
+            if(visited[j] == 0 && diff < min) {
+                min = diff;
+                pos = j;
             }
         }
-
-        visited[idx] = 1;
-        total += abs(head - req[idx]);
-        head = req[idx];
+        total += min;
+        head = req[pos];
+        visited[pos] = 1;
     }
-
-    printf("Total Seek Time (SSTF) = %d\n", total);
-    return 0;
+    printf("Total Seek Time = %d\n", total);
 }
