993fa8d7e9527a7d08c8c5df1d1e9fe7a86367d8
Joe <127773439+joegeorge022@users.noreply.github.com>
2026-05-25 22:56:12 +0530

    Update 7.Producer_Consumer.c

--- 7.Producer_Consumer.c
+++ 7.Producer_Consumer.c
@@ -1,109 +1,51 @@
 #include <stdio.h>
 
-int data = 0;
-int count = 0;
+int count = 0, size;
+int buffer[100];
 
-void reader(int id) {
-    count++;
-    printf("Reader %d is reading data = %d\n", id, data);
-    count--;
+void display() {
+    printf("Buffer: ");
+    for(int i = 0; i < count; i++) {
+        printf("%d ", buffer[i]);
+    }
+    printf("\n");
 }
 
-void writer(int id) {
-    if(count > 0) {
-        printf("Writer %d waiting\n", id);
+void reader() {
+    if(count == 0) {
+        printf("Buffer is empty\n");
     } else {
-        data++;
-        printf("Writer %d updated data to %d\n", id, data);
+        display();
+    }
+}
+
+void writer() {
+    if(count == size) {
+        printf("Buffer full\n");
+    } else {
+        buffer[count] = count + 1;
+        printf("Written: %d\n", buffer[count]);
+        count++;
+        display();
     }
 }
 
 int main() {
-    int choice, id;
+    int n;
+    printf("Enter buffer size: ");
+    scanf("%d", &size);
     while(1) {
-        printf("\n1.Reader\n2.Writer\n3.Exit\n");
-        printf("Enter choice: ");
-        scanf("%d", &choice);
-
-        switch(choice) {
+        printf("\nChoices are \n1.Reader\n2.Writer\n3.Exit\nChoice: ");
+        scanf("%d", &n);
+        switch(n) {
             case 1:
-                printf("Enter Reader ID: ");
-                scanf("%d", &id);
-                reader(id);
+                reader();
                 break;
-
             case 2:
-                printf("Enter Writer ID: ");
-                scanf("%d", &id);
-                writer(id);
+                writer();
                 break;
-
             case 3:
                 return 0;
-
-            default:
-                printf("Invalid choice\n");
-        }
-    }
-}
-
-//---------------------- WRITER PRIORITY------------------------//
-#include <stdio.h>
-
-int data = 0;
-int count = 0;
-int writerWaiting = 0;
-
-void reader(int id) {
-    if(writerWaiting) {
-        printf("Reader %d waiting\n", id);
-        return;
-    }
-
-    count++;
-    printf("Reader %d is reading data = %d\n", id, data);
-    count--;
-}
-
-void writer(int id) {
-    writerWaiting = 1;
-
-    if(count > 0) {
-        printf("Writer %d waiting\n", id);
-    } else {
-        data++;
-        printf("Writer %d updated data to %d\n", id, data);
-    }
-
-    writerWaiting = 0;
-}
-
-int main() {
-    int choice, id;
-
-    while(1) {
-        printf("\n1.Reader\n2.Writer\n3.Exit\n");
-        printf("Enter choice: ");
-        scanf("%d", &choice);
-
-        switch(choice) {
-            case 1:
-                printf("Enter Reader ID: ");
-                scanf("%d", &id);
-                reader(id);
-                break;
-
-            case 2:
-                printf("Enter Writer ID: ");
-                scanf("%d", &id);
-                writer(id);
-                break;
-
-            case 3:
-                return 0;
-
-            default:
-                printf("Invalid choice\n");
         }
     }
 }
