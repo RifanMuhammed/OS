6047f65549c844c5409c781d2e1b8bec4d39b217
Joe <127773439+joegeorge022@users.noreply.github.com>
2026-05-25 16:20:30 +0530

    Update 9.LRU.c

--- 9.LRU.c
+++ 9.LRU.c
@@ -1,55 +1,57 @@
 #include <stdio.h>
 
 int main() {
-    int n, f, i, j, faults = 0, time = 0;
-    int frames[10], used[10];
+
+    int n, f, i, j;
+    int pages[20], frames[10], used[10];
+    int faults = 0, found;
 
     printf("Enter number of pages: ");
     scanf("%d", &n);
 
-    int pages[n];
     printf("Enter reference string: ");
-    for(i = 0; i < n; i++) scanf("%d", &pages[i]);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &pages[i]);
+    }
 
     printf("Enter number of frames: ");
     scanf("%d", &f);
 
     for(i = 0; i < f; i++) {
         frames[i] = -1;
-        used[i] = 0;
+        used[i] = -1;
     }
 
     for(i = 0; i < n; i++) {
-        int found = 0;
+        found = 0;
 
         for(j = 0; j < f; j++) {
             if(frames[j] == pages[i]) {
                 found = 1;
-                used[j] = ++time;
+
+                // update recent use time
+                used[j] = i;
                 break;
             }
         }
 
-        if(!found) {
-            int min = 9999, idx = 0;
-
-            for(j = 0; j < f; j++) {
-                if(frames[j] == -1) {
-                    idx = j;
-                    break;
-                }
-                if(used[j] < min) {
-                    min = used[j];
-                    idx = j;
+        // page fault
+        if(found == 0) {
+            int lru = 0;
+            // find least recently used page
+            for(j = 1; j < f; j++) {
+                if(used[j] < used[lru]) {
+                    lru = j;
                 }
             }
 
-            frames[idx] = pages[i];
-            used[idx] = ++time;
+            frames[lru] = pages[i];
+            used[lru] = i;
             faults++;
         }
     }
 
-    printf("Page Faults (LRU) = %d\n", faults);
+    printf("Page Faults = %d", faults);
+
     return 0;
 }
