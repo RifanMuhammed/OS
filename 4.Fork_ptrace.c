fed4444be7f4aca7fcb12fd23e440fb83dab9c40
Joe <127773439+joegeorge022@users.noreply.github.com>
2026-05-26 22:16:55 +0530

    Create 4.Fork_ptrace.c

---
+++ 4.Fork_ptrace.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+
+int main() {
+    pid_t pid;
+    pid = fork();
+
+    if (pid < 0) {
+        printf("Fork failed\n");
+    } else if (pid == 0) {
+        
+        // Child process requests tracing
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        
+        printf("Child Process\n");
+        printf("PID: %d\n", getpid());
+        printf("Parent PID: %d\n", getppid());
+    } else {
+        wait(NULL);
+        
+        printf("Parent Process\n");
+        printf("PID: %d\n", getpid());
+        printf("Child PID: %d\n", pid);
+        
+        printf("Parent is tracing child using ptrace\n");
+    }
+
+    return 0;
+}
+
+
+// ptrace() system call is used for tracing processes.
+// PTRACE_TRACEME inside it means "This process should be traced by its parent."
