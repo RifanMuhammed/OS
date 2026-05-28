649e7f0383c2e8d78a2f17a4b6a5ae02a7c4ca84
RifanMuhammed <183482829+RifanMuhammed@users.noreply.github.com>
2026-05-28 23:44:22 +0530

    Update 6.RoundRobin.c

--- 6.RoundRobin.c
+++ 6.RoundRobin.c
@@ -1,59 +1,111 @@
 #include <stdio.h>
-//ROUND ROBIN SCHEDULING
+
+// ROUND ROBIN SCHEDULING
+
 int main() {
-    int n, i, time = 0, completed = 0, tq = 3;
-    int bt[10], at[10], rt[10], wt[10], tat[10], ct[10], done[10] = {0};
+
+    int totalProcesses, i;
+
+    int currentTime = 0;
+    int completedProcesses = 0;
+
+    int timeQuantum = 3;
+
+    int burstTime[10];
+    int arrivalTime[10];
+    int remainingTime[10];
+    int waitingTime[10];
+    int turnaroundTime[10];
+    int completionTime[10];
+
+    int finished[10] = {0};
 
     printf("Enter number of processes: ");
-    scanf("%d", &n);
+    scanf("%d", &totalProcesses);
 
-    for(i = 0; i < n; i++) {
-        printf("P%d Burst Arrival: ", i+1);
-        scanf("%d %d", &bt[i], &at[i]);
-        rt[i] = bt[i];
+    for(i = 0; i < totalProcesses; i++) {
+
+        printf("P%d Burst Arrival: ", i + 1);
+
+        scanf("%d %d",
+              &burstTime[i],
+              &arrivalTime[i]);
+
+        remainingTime[i] = burstTime[i];
     }
-//
-    while(completed < n) {
-        int found=0;
-        
-        for(i=0;i<n;i++){
-            if(at[i]<=time && done[i]==0){
-                found=1
-                
-                if(rt[i]>tq){
-                    rt[i] -=tq;
-                    time +=tq;
+
+    while(completedProcesses < totalProcesses) {
+
+        int processFound = 0;
+
+        for(i = 0; i < totalProcesses; i++) {
+
+            if(arrivalTime[i] <= currentTime &&
+               finished[i] == 0) {
+
+                processFound = 1;
+
+                if(remainingTime[i] > timeQuantum) {
+
+                    remainingTime[i] -= timeQuantum;
+
+                    currentTime += timeQuantum;
+
                 } else {
-                    time+=rt[i];
-                    ct[i]=time;
-                    rt[i]=0;
-                    done[i]=1;
-                    completed++;
+
+                    currentTime += remainingTime[i];
+
+                    completionTime[i] = currentTime;
+
+                    remainingTime[i] = 0;
+
+                    finished[i] = 1;
+
+                    completedProcesses++;
                 }
             }
         }
-        
-        if(found==0){
-            time++;
+
+        if(processFound == 0) {
+
+            currentTime++;
         }
     }
-//    
-    for(i = 0; i < n; i++) {
-        tat[i] = ct[i] - at[i];
-        wt[i] = tat[i] - bt[i];
+
+    for(i = 0; i < totalProcesses; i++) {
+
+        turnaroundTime[i] =
+            completionTime[i] - arrivalTime[i];
+
+        waitingTime[i] =
+            turnaroundTime[i] - burstTime[i];
     }
 
-    float avg = 0;
-    for(i = 0; i < n; i++) {
-        avg += wt[i];
+    float averageWaitingTime = 0;
+
+    for(i = 0; i < totalProcesses; i++) {
+
+        averageWaitingTime += waitingTime[i];
     }
-    avg /= n;
+
+    averageWaitingTime /= totalProcesses;
 
     printf("\nProcess\tAT\tBT\tCT\tTAT\tWT\n");
-    for(i = 0; i < n; i++) {
-        printf("P%d\t\t%d\t%d\t%d\t%d\t%d\n",i+1, at[i], bt[i], ct[i], tat[i], wt[i]);
+
+    for(i = 0; i < totalProcesses; i++) {
+
+        printf("P%d\t\t%d\t%d\t%d\t%d\t%d\n",
+
+               i + 1,
+               arrivalTime[i],
+               burstTime[i],
+               completionTime[i],
+               turnaroundTime[i],
+               waitingTime[i]);
     }
-    printf("\nAvg Waiting Time = %.2f\n", avg);
+
+    printf("\nAvg Waiting Time = %.2f\n",
+           averageWaitingTime);
 
     return 0;
 }
