67de8853ae8f9e1c5459fca379f922ff94583dd9
RifanMuhammed <183482829+RifanMuhammed@users.noreply.github.com>
2026-05-29 06:54:15 +0530

    Update 9.FIFO.c

--- 9.FIFO.c
+++ 9.FIFO.c
@@ -66,3 +66,32 @@ int main() {
     printf("Page Faults = %d",
            pageFaults);
 }
+
+
+FIFO Page Replacement Algorithm
+Start
+Input number of pages
+Input reference string
+Input number of frames
+Initialize all frames as empty
+Set:
+pageFaults = 0
+replaceIndex = 0
+
+Repeat for each page in reference string:
+
+a. Assume page is not found
+
+b. Check all frames:
+
+If page already exists in frame:
+Mark page as found
+Stop checking
+
+c. If page is not found:
+
+Replace page at replaceIndex
+Move replaceIndex to next frame
+Increase page fault count
+Display total page faults
+Stop
