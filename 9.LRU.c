ed48a6f0ba3a2d863d6bcb42f6f8d691786e6e2c
RifanMuhammed <183482829+RifanMuhammed@users.noreply.github.com>
2026-05-29 06:55:42 +0530

    Update 9.LRU.c

--- 9.LRU.c
+++ 9.LRU.c
@@ -1,50 +1,110 @@
 #include <stdio.h>
-// LRU 
+
+// LRU PAGE REPLACEMENT
+
 int main() {
-    int n, f, i, j;
-    int pages[20], frames[10], used[10];
-    int faults = 0, found;
+
+    int totalPages, totalFrames, i, j;
+
+    int pageReference[20];
+    int memoryFrames[10];
+
+    int lastUsed[10];
+
+    int pageFaults = 0;
+
+    int pageFound;
 
     printf("Enter number of pages: ");
-    scanf("%d", &n);
+    scanf("%d", &totalPages);
 
     printf("Enter reference string: ");
-    for(i = 0; i < n; i++) {
-        scanf("%d", &pages[i]);
+
+    for(i = 0; i < totalPages; i++) {
+
+        scanf("%d", &pageReference[i]);
     }
 
     printf("Enter number of frames: ");
-    scanf("%d", &f);
+    scanf("%d", &totalFrames);
 
-    for(i = 0; i < f; i++) {
-        frames[i] = -1;
-        used[i] = -1;                       //additional
+    for(i = 0; i < totalFrames; i++) {
+
+        memoryFrames[i] = -1;
+
+        lastUsed[i] = -1;
     }
-//
-    for(i = 0; i < n; i++) {
-        found = 0;
 
-        for(j = 0; j < f; j++) {
-            if(frames[j] == pages[i]) {
-                found = 1;
-                used[j] = i;                //additional
+    for(i = 0; i < totalPages; i++) {
+
+        pageFound = 0;
+
+        for(j = 0; j < totalFrames; j++) {
+
+            if(memoryFrames[j] ==
+               pageReference[i]) {
+
+                pageFound = 1;
+
+                lastUsed[j] = i;
+
                 break;
             }
         }
 
-        if(found == 0) {
-            int lru = 0;
-            for(j = 1; j < f; j++) {
-                if(used[j] < used[lru]) {
-                    lru = j;
+        if(pageFound == 0) {
+
+            int leastRecentlyUsed = 0;
+
+            for(j = 1; j < totalFrames; j++) {
+
+                if(lastUsed[j] <
+                   lastUsed[leastRecentlyUsed]) {
+
+                    leastRecentlyUsed = j;
                 }
             }
 
-            frames[lru] = pages[i];
-            used[lru] = i;
-            faults++;
+            memoryFrames[leastRecentlyUsed] =
+                pageReference[i];
+
+            lastUsed[leastRecentlyUsed] = i;
+
+            pageFaults++;
         }
     }
-//
-    printf("Page Faults = %d", faults);
+
+    printf("Page Faults = %d",
+           pageFaults);
+
+    return 0;
 }
+
+
+
+LRU Algorithm
+Start
+Input number of pages
+Input reference string
+Input number of frames
+Initialize:
+all frames as empty
+all last used values as -1
+Set page faults = 0
+
+Repeat for every page in reference string:
+
+a. Check whether page already exists in frames
+
+b. If page exists:
+
+Update its last used position
+
+c. Else:
+
+Find least recently used page
+Replace it with new page
+Update last used position
+Increase page faults
+Display total page faults
+Stop
