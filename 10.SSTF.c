fa302801ced523e3428bb993f2ee0c6b012a716c
RifanMuhammed <183482829+RifanMuhammed@users.noreply.github.com>
2026-05-29 06:59:28 +0530

    Update 10.SSTF.c

--- 10.SSTF.c
+++ 10.SSTF.c
@@ -2,34 +2,81 @@
 #include <stdlib.h>
 
 int main() {
-    int n, head, total = 0;
-    int req[20], visited[20] = {0};
+
+    int totalRequests;
+
+    int headPosition;
+
+    int totalSeekTime = 0;
+
+    int diskRequests[20];
+
+    int completed[20] = {0};
 
     printf("Enter number of requests: ");
-    scanf("%d", &n);
+    scanf("%d", &totalRequests);
 
     printf("Enter requests: ");
-    for(int i = 0; i < n; i++){
-        scanf("%d", &req[i]);
+
+    for(int i = 0; i < totalRequests; i++) {
+
+        scanf("%d", &diskRequests[i]);
     }
 
     printf("Enter initial head position: ");
-    scanf("%d", &head);
-//
-    for(int i = 0; i < n; i++) {
-        int min = 9999,pos = -1;
+    scanf("%d", &headPosition);
 
-        for(int j = 0; j < n; j++) {
-            int diff = abs(head - req[j]);
-            if(visited[j] == 0 && diff < min) {
-                min = diff;
-                pos = j;
+    for(int i = 0; i < totalRequests; i++) {
+
+        int minimumDistance = 9999;
+
+        int nearestRequest = -1;
+
+        for(int j = 0; j < totalRequests; j++) {
+
+            int distance =
+                abs(headPosition - diskRequests[j]);
+
+            if(completed[j] == 0 &&
+               distance < minimumDistance) {
+
+                minimumDistance = distance;
+
+                nearestRequest = j;
             }
         }
-        total += min;
-        head = req[pos];
-        visited[pos] = 1;
+
+        totalSeekTime += minimumDistance;
+
+        headPosition =
+            diskRequests[nearestRequest];
+
+        completed[nearestRequest] = 1;
     }
-//
-    printf("Total Seek Time = %d\n", total);
+
+    printf("Total Seek Time = %d\n",
+           totalSeekTime);
+
+    return 0;
 }
+
+
+SSTF Algorithm
+Start
+Input number of disk requests
+Input request queue
+Input initial head position
+Mark all requests as unvisited
+
+Repeat until all requests are completed:
+
+a. Find request nearest to current head position
+
+b. Move head to nearest request
+
+c. Add seek distance to total seek time
+
+d. Mark request as completed
+
+Display total seek time
+Stop
