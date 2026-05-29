74a0530ff9c6f59f974c4eadf7c9ed0c3e4404a8
RifanMuhammed <183482829+RifanMuhammed@users.noreply.github.com>
2026-05-29 06:57:51 +0530

    Update 10.CSCAN.c

--- 10.CSCAN.c
+++ 10.CSCAN.c
@@ -2,49 +2,122 @@
 #include <stdlib.h>
 
 int main() {
-    int n, head, total = 0;
-    int req[20];
+
+    int totalRequests;
+
+    int headPosition;
+
+    int totalSeekTime = 0;
+
+    int diskRequests[20];
 
     printf("Enter number of requests: ");
-    scanf("%d", &n);
+    scanf("%d", &totalRequests);
 
     printf("Enter requests: ");
-    for(int i = 0; i < n; i++){
-        scanf("%d", &req[i]);
-    }    
-        
-    printf("Enter initial head position: ");
-    scanf("%d", &head);
 
-    for(int i = 0; i < n-1; i++) {
-        for(int j = i+1; j < n; j++) {
-            if(req[i] > req[j]) {
-                int temp = req[i];
-                req[i] = req[j];
-                req[j] = temp;
+    for(int i = 0; i < totalRequests; i++) {
+
+        scanf("%d", &diskRequests[i]);
+    }
+
+    printf("Enter initial head position: ");
+    scanf("%d", &headPosition);
+
+    // Sort requests
+
+    for(int i = 0; i < totalRequests - 1; i++) {
+
+        for(int j = i + 1; j < totalRequests; j++) {
+
+            if(diskRequests[i] >
+               diskRequests[j]) {
+
+                int temp = diskRequests[i];
+
+                diskRequests[i] =
+                    diskRequests[j];
+
+                diskRequests[j] = temp;
             }
         }
     }
 
-    int idx = 0;
-    while(idx < n && req[idx] < head)
-        idx++;
-//
-    // move right
-    for(int i = idx; i < n; i++) {
-        total += abs(head - req[i]);
-        head = req[i];
+    int startIndex = 0;
+
+    while(startIndex < totalRequests &&
+          diskRequests[startIndex] < headPosition)
+
+        startIndex++;
+
+    // Move right
+
+    for(int i = startIndex;
+        i < totalRequests; i++) {
+
+        totalSeekTime +=
+            abs(headPosition - diskRequests[i]);
+
+        headPosition =
+            diskRequests[i];
     }
-    // jump to beginning
-    if(idx > 0) {
-        total += abs(head - req[0]);
-        head = req[0];
+
+    // Jump to beginning
+
+    if(startIndex > 0) {
+
+        totalSeekTime +=
+            abs(headPosition - diskRequests[0]);
+
+        headPosition =
+            diskRequests[0];
     }
-    // continue right again
-    for(int i = 0; i < idx; i++) {
-        total += abs(head - req[i]);
-        head = req[i];
+
+    // Continue moving right
+
+    for(int i = 0; i < startIndex; i++) {
+
+        totalSeekTime +=
+            abs(headPosition - diskRequests[i]);
+
+        headPosition =
+            diskRequests[i];
     }
-//
-    printf("Total Seek Time (CSCAN) = %d\n", total);
+
+    printf("Total Seek Time (CSCAN) = %d\n",
+           totalSeekTime);
+
+    return 0;
 }
+
+
+
+
+CSCAN Algorithm
+Start
+Input number of disk requests
+Input disk request queue
+Input initial head position
+Sort all requests in ascending order
+Find first request greater than current head position
+Move disk head to the right:
+Service all higher requests
+Add seek distance
+Jump to beginning of disk
+Continue servicing remaining requests from beginning
+Calculate total seek time
+Display total seek time
+Stop
+CSCAN Meaning
+
+CSCAN = Circular SCAN
+
+Disk head moves only in one direction.
+
+After reaching end:
+
+Head jumps back to beginning
+
+Then continues again.
+
+Like a circular elevator system.
